631dc997ab363dc6bda78cbef28c3c17848cb178
Tanvi <145559687+TanviS001@users.noreply.github.com>
2025-08-31 11:01:02 +0530

    Update ContainsDuplicate.py

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -9,3 +9,18 @@ class Solution:
             else:
                 return True
         return False
+
+
+# alternative (worse) o(nlogn) space complexity due to Timsort 
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        s=sorted(nums)
+        if len(nums)<1:
+            return False
+
+        for i in range(0,len(s)):
+            if i+1<len(s) and s[i]==s[i+1]:
+                return True
+            
+        return False
